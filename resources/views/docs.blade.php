aa5c3e2c3a2b1bd9be4cccd438dc4ee09b24daa0
Phil Sturgeon <me@philsturgeon.com>
2021-07-05 16:19:45 +0200

    Update elements to v7.0 stable

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ config('stoplight.title') }}</title>
     <!-- Embed elements Elements via Web Component -->
-    <script src="https://unpkg.com/@stoplight/elements@beta/web-components.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@beta/styles.min.css">
+    <script src="https://unpkg.com/@stoplight/elements@v7/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@v7/styles.min.css">
   </head>
   <body>
 
